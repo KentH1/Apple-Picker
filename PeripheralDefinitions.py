fac016173c8bdf65505a69d536298dbbcaa03aef
Kent Hawkins <kent@trkl.co>
2020-09-01 16:44:32 +1200

    Updated Classes
    Created Stepper Class and updated servo class

--- PeripheralDefinitions.py
+++ PeripheralDefinitions.py
@@ -1,33 +1,18 @@
 #Peripheral Definitions
 print("Start Setup")
-import RPi.GPIO as GPIO
+import RPi.GPIO_0_7_0 as GPIO
 import time
 GPIO.setmode(GPIO.BCM) #Sets reference to pins to be their GPIO __ number
-#-----------Servos-----------
-servoA1_PIN = 5
-servoA2_PIN = 6
-servoA3_PIN = 12
-wrist_PIN = 23
-grip_PIN = 24
 
 
-GPIO.setup(servoA1_PIN, GPIO.OUT)
-GPIO.setup(servoA2_PIN, GPIO.OUT)
-GPIO.setup(servoA3_PIN, GPIO.OUT)
-GPIO.setup(wrist_PIN, GPIO.OUT)
-GPIO.setup(grip_PIN, GPIO.OUT)
-
-pwm1 = GPIO.PWM(servoA1_PIN, 50) # GPIO 17 for PWM with 50Hz
-pwm2 = GPIO.PWM(servoA2_PIN, 50) # GPIO 17 for PWM with 50Hz
-pwm3 = GPIO.PWM(servoA3_PIN, 50) # GPIO 17 for PWM with 50Hz
-pwm4 = GPIO.PWM(wrist_PIN, 50) # GPIO 17 for PWM with 50Hz
-pwm5 = GPIO.PWM(grip_PIN, 50) # GPIO 17 for PWM with 50Hz
 
 #-----------Stepper-----------
+from RpiMotorLib import RpiMotorLib
+
 stepperPINS = [4,17,27,22] #[2B,2A,1A,1B]
-for pin in stepperPINS:
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, 0)
+#for pin in stepperPINS:
+#  GPIO.setup(pin, GPIO.OUT)
+#  GPIO.output(pin, 0)
 
 halfstep_seq = [
   [1,0,0,0],
--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 import PeripheralDefinitions
 import analogSetup
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 #import SPI_Trial_Setup
+#-----------Servo Setup-----------
+class main_joint:
+    def __init__(self,servoPin,freq):
+        self.servoPin=servoPin
+        self.pwm=0
+    def servo_setup(self):
+        GPIO.setup(self.servoPin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.servoPin, self.freq) # GPIO pin for PWM with 50Hz
+        self.pwm.start(0)
+    def move(self,angle):
+        duty=self.angle/18+2
+        self.pwm.ChangeDutyCycle(duty)
 
+j1=main_joint(5,50)
+j2=main_joint(6,50)
+j3=main_joint(12,50)
+j4=main_joint(23,50)
+j5=main_joint(24,50)
+j1.servo_setup()
+j2.servo_setup()
+j3.servo_setup()
+j4.servo_setup()
+j5.servo_setup()
+
+#-----------Stepper Setup-----------
+class slider:
+    def __init__(self,stepperPINS):
+        self.stepperPINS= stepperPINS
+    def stepper_setup(self):
+        GPIO.setup(self.stepperPINS, GPIO.OUT)
+        GPIO.output(self.stepperPINS, 0)
+    def move(self):
+        GPIO.output(self.stepperPINS, 1)
+
+#-----------X-----------
 def main():
-        
+    print("Hello World")
+#def home():
+
+def stop():
+    j1.pwm.stop()
+    GPIO.cleanup()
